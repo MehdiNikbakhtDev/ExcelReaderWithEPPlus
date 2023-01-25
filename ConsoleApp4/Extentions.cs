79ef052e611e8e81885f4ea7525866f8ae053b7d
MehdiNikbakht <mahdinikbakht7105@gmail.com>
2023-01-25 19:56:24 +0330

    Init

---
+++ ConsoleApp4/Extentions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public static class Extentions
+    {
+        public static int? ToInt(this string str)
+        {
+            try
+            {
+                int _str = 0;
+                var isDone = int.TryParse(str, out _str);
+                if (isDone)
+                    return _str;
+                else return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public static long? ToLong(this string str)
+        {
+            try
+            {
+                long _str = 0;
+                var isDone = long.TryParse(str, out _str);
+                if (isDone)
+                    return _str;
+                else return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -11,8 +13,44 @@ namespace ConsoleApp4
         {
 
 
-            string path = @"~\1.xlsx";
+            string path = @".\1.xlsx";
+            #region [چک موجود بودن فایل اکسل]
 
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File Not Exist.");
+                return;
+            }
+            #endregion [چک موجود بودن فایل اکسل]
+
+            #region [شروع خواندن اکسل]
+            Stream stream = new StreamReader(path).BaseStream;
+
+            var excel = new ExcelPackage(stream);
+            var sheet = excel.Workbook.Worksheets.FirstOrDefault();
+            var recordList = new List<DataModel>();
+            for (int i = 2; i <= sheet.Dimension.End.Row; i++)
+            {
+                var record = new DataModel();
+                record.sharh = sheet.Cells[i, 1].Value.ToString();
+                record.count = sheet.Cells[i, 2].Value.ToString().ToInt();
+                record.Amount = sheet.Cells[i, 3].Value.ToString().ToLong();
+                record.TotalAmount = sheet.Cells[i, 4].Value.ToString().ToLong();
+                if (string.IsNullOrEmpty(record.sharh) || !record.count.HasValue || !record.Amount.HasValue || !record.TotalAmount.HasValue)
+                    break;
+
+                recordList.Add(record);
+
+
+            }
+            #endregion [پایان خواندن اکسل]
+            #region [چاپ فایل اکسل]
+            foreach (var item in recordList)
+            {
+                Console.WriteLine($"Sharh: {item.sharh }\t Amount: {item.Amount}\t Count: {item.count}\t TotalAmount:{item.TotalAmount}");
+            }
+            #endregion [چاپ فایل اکسل]
+            Console.ReadKey();
         }
     }
 }
