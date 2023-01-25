51ef229fcf8b6b644f8335d346a44301b039c522
MehdiNikbakht <mahdinikbakht7105@gmail.com>
2023-01-25 19:34:14 +0330

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string path = @"~\1.xlsx";
+
+        }
+    }
+}
